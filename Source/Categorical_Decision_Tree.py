3ad87e19ade188c47b8a173f5c4e8cb8f59a146f
ChanakyaVishal <chanakyavishal2@gmail.com>
2019-01-15 19:15:38 +0530

    Updated termination conditions

--- Source/Categorical_Decision_Tree.py
+++ Source/Categorical_Decision_Tree.py
@@ -1,9 +1,8 @@
 import math
 import pandas as pd
-import numpy as np
 
 
-data = pd.read_csv('C:\\Users\\Chanakya\\Desktop\\Sem VI\\SMAI\\Assignment 1\\Data_Set\\train.csv')
+data = pd.read_csv('C:\\Users\\Chanakya\\Desktop\\Sem VI\\SMAI\\Decision_tree\\Data_Set\\train.csv')
 
 #Train-Validation data Split
 data_size = data.size
@@ -11,23 +10,46 @@ train_data_size = int(data_size * 0.08)
 train_data = data[0:train_data_size]
 
 
-def build_descision_tree(data_points):
+def build_decision_tree(data_points):
+    #Get Optimal Attribute
+    attribute = attribute_selection(data_points)
+    unique_values = data_points[attribute].unique()
+    for node in unique_values:
+        new_data_points = data_points[data_points[attribute] == node].drop(attribute, axis = 1)
+        print(new_data_points)
+    return attribute
+
+
+def terminate_condition(data_points):
     feature_list = list(data_points.columns.values)
-    feature_list_len = feature_list.__len__() - 1
 
+    # Terminating conditions
+    if data_points.size == 0:
+        return 1
 
+    elif feature_list.__len__() == 1:
+        # Majority voting
+        positive_points = data_points['left'][data_points.left == 1].count()
+        negative_points = data_points['left'][data_points.left == 0].count()
+        return positive_points > negative_points
 
+    elif data_points[data_points['left'] == 1]['left'].size == data_points.size/data_points.columns.size:
+        return 1
+    elif data_points[data_points['left'] == 0]['left'].size == data_points.size/data_points.columns.size:
+        return 0
 
+    else:
+        return -1
 
 def attribute_selection(data_points):
     feature_list = list(data_points.columns.values)
     minimum_value = 0
     minimum_attribute = ''
-    len = feature_list.__len__()
-    for i in range (0,len - 1):
+    feature_list_len = feature_list.__len__()
+    for i in range (0, feature_list_len - 1):
         data_points_of_attribute = data_points[[feature_list[i], 'left']]
         info_after_attribute = info_of_attribute(data_points_of_attribute, feature_list[i])[feature_list[i]]
-        if(minimum_value < info_after_attribute):
+        if minimum_value < info_after_attribute:
             minimum_value = info_after_attribute
             minimum_attribute = feature_list[i]
     return minimum_attribute
--- Test/test_categorical_decision_tree.py
+++ Test/test_categorical_decision_tree.py
@@ -18,7 +18,19 @@ def test_info_of_attribute():
     output = dt.info_of_attribute(df, 'A')
     print(output)#0.95 correct value
 
+def test_build_decision_tree():
+    df = pd.DataFrame({'A': [0, 1, 1, 1, 0], 'B': [1, 1, 1, 1, 0], 'left': [1, 0, 1, 1, 0]})
+    output = dt.build_decision_tree(df)
+    print(output)
+
+def test_terminate_condition():
+    df = pd.DataFrame({'A': [0, 1, 1, 1, 0], 'B': [1, 1, 1, 1, 0], 'left': [1, 1, 1, 1, 1]})
+    output = dt.terminate_condition(df)
+    print(output)
+
 
 #test_total_info()
-test_attribute_selection()
+#test_attribute_selection()
 #test_info_of_attribute()
+#test_build_decision_tree()
+test_terminate_condition()
