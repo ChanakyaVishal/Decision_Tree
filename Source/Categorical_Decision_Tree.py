53c8295d1af4ee7111b5d5e0b41f49f9956da9f4
ChanakyaVishal <chanakyavishal2@gmail.com>
2019-01-15 20:48:18 +0530

    Working code for generating decision trees for categorical data

--- Source/Categorical_Decision_Tree.py
+++ Source/Categorical_Decision_Tree.py
@@ -1,52 +1,67 @@
 import math
 import pandas as pd
 
-
 data = pd.read_csv('C:\\Users\\Chanakya\\Desktop\\Sem VI\\SMAI\\Decision_tree\\Data_Set\\train.csv')
 
-#Train-Validation data Split
+# Train-Validation data Split
 data_size = data.size
 train_data_size = int(data_size * 0.08)
 train_data = data[0:train_data_size]
 
+decision_tree = {}
 
-def build_decision_tree(data_points):
-    #Get Optimal Attribute
+
+def build_decision_tree(data_points, previous_node_value):
+    terminate_value = terminate_condition(data_points)
+    if terminate_value != -1:
+        return previous_node_value, terminate_value
+
+    # Get Optimal Attribute
     attribute = attribute_selection(data_points)
     unique_values = data_points[attribute].unique()
+    intermediate_tree = {}
     for node in unique_values:
-        new_data_points = data_points[data_points[attribute] == node].drop(attribute, axis = 1)
-        print(new_data_points)
-    return attribute
+        new_data_points = data_points[data_points[attribute] == node].drop(attribute, axis=1)
+        intermediate_node_value = build_decision_tree(new_data_points, node)
+        if intermediate_node_value is not None:
+            intermediate_tree[intermediate_node_value[0]] = intermediate_node_value[1]
+    intermediate_tree_with_key = [attribute, intermediate_tree]
+    return intermediate_tree_with_key
 
 
 def terminate_condition(data_points):
     feature_list = list(data_points.columns.values)
+    print(feature_list)
 
     # Terminating conditions
     if data_points.size == 0:
+        #print("Sample size 0")
         return 1
 
     elif feature_list.__len__() == 1:
+        #print("No more features")
         # Majority voting
         positive_points = data_points['left'][data_points.left == 1].count()
         negative_points = data_points['left'][data_points.left == 0].count()
         return positive_points > negative_points
 
-    elif data_points[data_points['left'] == 1]['left'].size == data_points.size/data_points.columns.size:
+    elif data_points[data_points['left'] == 1]['left'].size == data_points.size / data_points.columns.size:
+        #print("Homogenity 1")
         return 1
-    elif data_points[data_points['left'] == 0]['left'].size == data_points.size/data_points.columns.size:
+    elif data_points[data_points['left'] == 0]['left'].size == data_points.size / data_points.columns.size:
+        #print("Homogenity 0")
         return 0
 
     else:
         return -1
+
 
 def attribute_selection(data_points):
     feature_list = list(data_points.columns.values)
     minimum_value = 0
     minimum_attribute = ''
     feature_list_len = feature_list.__len__()
-    for i in range (0, feature_list_len - 1):
+    for i in range(0, feature_list_len - 1):
         data_points_of_attribute = data_points[[feature_list[i], 'left']]
         info_after_attribute = info_of_attribute(data_points_of_attribute, feature_list[i])[feature_list[i]]
         if minimum_value < info_after_attribute:
@@ -62,16 +77,18 @@ def info_of_attribute(data_points, feature):
     for i in unique_feature_values:
         temp_df = data_points[data_points[feature] == i]
         info = total_info(temp_df)
-        info_a += info * data_points[data_points[feature] == i].count()/total_len
+        info_a += info * data_points[data_points[feature] == i].count() / total_len
     return -info_a
 
+
 def total_info(data_points):
-    #For a binary classification problem
+    # For a binary classification problem
     positive_points = data_points['left'][data_points.left == 1].count()
     negative_points = data_points['left'][data_points.left == 0].count()
     total_points = positive_points + negative_points
 
-    #Formula to calculate the value
-    positive_point_ratio = positive_points/total_points
-    negative_point_ratio = negative_points/total_points
-    return -(positive_point_ratio*math.log2(positive_point_ratio + 1) + negative_point_ratio*math.log2(negative_point_ratio + 1))
+    # Formula to calculate the value
+    positive_point_ratio = positive_points / total_points
+    negative_point_ratio = negative_points / total_points
+    return -(positive_point_ratio * math.log2(positive_point_ratio + 1) + negative_point_ratio * math.log2(
+        negative_point_ratio + 1))
--- Test/test_categorical_decision_tree.py
+++ Test/test_categorical_decision_tree.py
@@ -2,10 +2,12 @@ import pandas as pd
 import numpy as np
 import Source.Categorical_Decision_Tree as dt
 
+
 def test_total_info():
     df = pd.DataFrame({'A': [1, 1, 1, 1], 'left': [1, 0, 1, 1]})
     output = dt.total_info(df)
-    print(output) #0.811 correct value
+    print(output)  # 0.811 correct value
+
 
 def test_attribute_selection():
     df = pd.DataFrame({'A': [0, 1, 1, 1, 0], 'B': [1, 1, 1, 1, 0], 'left': [1, 0, 1, 1, 0]})
@@ -13,15 +15,18 @@ def test_attribute_selection():
     print('output')
     print(output)
 
+
 def test_info_of_attribute():
-    df = pd.DataFrame({'A': [0, 1, 1, 1, 0] ,'left': [1, 0, 1, 1, 0]})
+    df = pd.DataFrame({'A': [0, 1, 1, 1, 0], 'left': [1, 0, 1, 1, 0]})
     output = dt.info_of_attribute(df, 'A')
-    print(output)#0.95 correct value
+    print(output)  # 0.95 correct value
+
 
 def test_build_decision_tree():
     df = pd.DataFrame({'A': [0, 1, 1, 1, 0], 'B': [1, 1, 1, 1, 0], 'left': [1, 0, 1, 1, 0]})
-    output = dt.build_decision_tree(df)
+    output = dt.build_decision_tree(df, 'root')
     print(output)
+
 
 def test_terminate_condition():
     df = pd.DataFrame({'A': [0, 1, 1, 1, 0], 'B': [1, 1, 1, 1, 0], 'left': [1, 1, 1, 1, 1]})
@@ -29,8 +34,8 @@ def test_terminate_condition():
     print(output)
 
 
-#test_total_info()
-#test_attribute_selection()
-#test_info_of_attribute()
-#test_build_decision_tree()
-test_terminate_condition()
+# test_total_info()
+# test_attribute_selection()
+# test_info_of_attribute()
+test_build_decision_tree()
+# test_terminate_condition()
